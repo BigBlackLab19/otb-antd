162c61219b776ec9e73d65d15db171f5b82f32de
Carl Reyes <carlthebarnacle@gmail.com>
2021-07-28 14:17:56 +0800

    Fixed input and submit on modal upon pause

--- src/components/InterruptModal.js
+++ src/components/InterruptModal.js
@@ -1,29 +1,41 @@
 import React, { useState } from "react";
 import { Button, Modal, Form, Input } from "antd";
+import styled from "styled-components";
+
+const { TextArea } = Input;
 
 const InterruptModal = (props) => {
-  const { isOpen, toggle }  = props;
-  const [form] = Form.useForm();
- 
+  const { isOpen, toggle } = props;
+  const [reasoning, setReasoning] = useState("");
+
+  function handleChangeReasoning(e) {
+    setReasoning(e.target.value);
+  }
 
   return (
-    <Modal title="Paused" visible={isOpen} onCancel={toggle} onOk={toggle}>
-      <Form form={form} layout="vertical">
-        <Form.Item
-          name="reason"
-          label="Reason for Pausing"
-          rules={[
-            {
-              required: true,
-              message: "Please input the reason for pausing.",
-            },
-          ]}
-        >
-          <Input type="textarea" />
-        </Form.Item>
-      </Form>
+    <Modal
+      title="Current Task: {taskTitle} (Paused)"
+      visible={isOpen}
+      closable={false}
+      footer={null}
+    >
+      <TextArea
+        onChange={handleChangeReasoning}
+        placeholder="You paused the timer. Tell us what happened."
+      />
+      <SubmitButtonContainer>
+        <Button type="primary" onClick={toggle} disabled={!reasoning}>
+          Submit
+        </Button>
+      </SubmitButtonContainer>
     </Modal>
   );
 };
 
 export default InterruptModal;
+
+const SubmitButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  padding-top: 1.5em;
+`;
